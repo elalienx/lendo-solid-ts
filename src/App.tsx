38963ca7179ad30eb4800b0bbe0aee4487053bc7
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-30 21:14:00 +0100

    starting to create home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { Router, Route } from "@solidjs/router";
 
 // Project files
-import NavigationBar from "components/navigation-bar/NavigationBar";
 import { items } from "data/inventory.json";
 import Checkout from "pages/checkout/Checkout";
 import Home from "pages/home/Home";
@@ -11,15 +10,14 @@ import Product from "pages/product/Product";
 
 export default function App() {
   // Properties
-  const HomePage = <Home data={items} />;
-  const ProductPage = <Product data={items} />;
+  const HomePage = () => <Home data={items} />;
+  const ProductPage = () => <Product data={items} />;
 
   return (
     <div class="app">
       <Router>
-        {/* <NavigationBar number={5} /> */}
-        <Route path="/" component={() => HomePage} />
-        <Route path="/product/:id" component={() => ProductPage} />
+        <Route path="/" component={HomePage} />
+        <Route path="/product/:id" component={ProductPage} />
         <Route path="/checkout" component={Checkout} />
         <Route path="*" component={Page404} />
       </Router>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,22 @@
 // Project files
+import ItemProduct from "components/item-product/ItemProduct";
+import NavigationBar from "components/navigation-bar/NavigationBar";
 import type Product from "types/Product";
+import "./home.css";
 
 interface Props {
   data: Product[];
 }
 
 export default function Home({ data }: Props) {
-  return <h1>Home</h1>;
+  // Components
+  const Items = data.map((item) => <ItemProduct item={item} />);
+
+  return (
+    <div id="home" class="page">
+      <NavigationBar number={5} />
+      <h1>Our products</h1>
+      <section class="grid">{Items}</section>
+    </div>
+  );
 }
